16be823589d3bf04e4ce44a1324a831cdda1848c
Camilo_AB <camiloab97@gmail.com>
2023-05-01 12:26:56 -0500

    Proyecto terminado

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import './article.css';
 
-const Article = () => {
+const Article = ({imgUrl,date,title}) => {
   return (
-    <div>Article</div>
+    <div className='gpt3__blog-container__article'>
+      <div className='gpt3__blog-container__article-image'>
+        <img src={imgUrl} alt="Blog" />
+      </div>
+      <div className='gpt3__blog-container__article-content'>
+        <p>{date}</p>
+        <h3>{title}</h3>
+        <p>Read Full Article</p> 
+      </div>
+    </div>
   )
 }
 
